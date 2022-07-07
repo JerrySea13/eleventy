103bccff48646b232ac7fce341d06bb28a1f2970
SeaJerry <jsea@kindsys.us>
2022-07-07 14:27:24 -0700

    Add css to layouts

---
+++ _site/.eleventy.js
@@ -0,0 +1,15 @@
+// Config file
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("")
+    return {
+        dir: {
+            input: "src",
+            includes: "_includes",
+            output: "_site",
+        },
+        templateFormats: [ "md", "njk", "html"],
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+    };
+}
